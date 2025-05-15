6a561b5c8aa60afe815e1e85266002596047186a
Nirmit P <nirmitpatel16620@gmail.com>
2025-05-15 16:14:44 -0500

    updated about content

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,7 @@ export default function About() {
             <main>
                 <section className="about">
                     <h1>About Me</h1>
-                    <p>Hi! I'm Nirmit, a <b>UX designer and developer</b> passionate about creating intuitive and visually compelling digital experiences. Currently pursuing a <b>Master’s in Computer Science</b> at Oregon State University, I blend technical skills with a love for art and design.</p>
+                    <p>Hi! I'm Nirmit, a <b>UX designer and Frontend developer</b> passionate about creating intuitive and visually compelling digital experiences. Completed my <b>Master’s in Computer Science</b> at Oregon State University, I blend technical skills with a love for art and design.</p>
                     <p>My experience includes developing <b>fluid simulations</b>, <b>responsive web interfaces</b>, and <b>UX case studies</b> grounded in real <b>user research</b>. When I’m not coding or designing, I enjoy painting, exploring nature, and photography.</p>
                 </section>
             </main>
